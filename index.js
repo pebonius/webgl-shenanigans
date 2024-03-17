d1a6804938615367801eb7159d5085eaf2729174
pebonius <127553094+pebonius@users.noreply.github.com>
2024-03-17 00:49:51 +0000

    change shtuff

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+import { drawScene } from "./drawScene.js";
+import { initBuffers } from "./initBuffers.js";
+
 const canvas = document.getElementById("canvas");
 const gl = canvas.getContext("webgl");
-const vertexShaderInput = document.getElementById("vertShader");
 const fragmentShaderInput = document.getElementById("fragShader");
 const runButton = document.getElementById("runShaders");
 
 function getVertexShaderSource() {
-  return vertexShaderInput.value;
+  return `attribute vec4 aVertexPosition;
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }`;
 }
 
 function getFragmentShaderSource() {
@@ -27,7 +34,7 @@ function loadShader(gl, type, source) {
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert(
-      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
+      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`
     );
     gl.deleteShader(shader);
     return null;
@@ -36,17 +43,26 @@ function loadShader(gl, type, source) {
   return shader;
 }
 
-function setupProgram(vertexShader, fragmentShader) {
+function initShaderProgram(vertexShader, fragmentShader) {
   const program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
   gl.useProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    alert(
+      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+        program
+      )}`
+    );
+    return null;
+  }
+
   return program;
 }
 
 function runShaders() {
-  // check if webgl is working
   if (gl === null) {
     alert("webgl not working, probably your browser's fault :)");
     return;
@@ -56,28 +72,36 @@ function runShaders() {
   const fragmentShaderSource = getFragmentShaderSource();
 
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  const fragmentShader = loadShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource
+  );
 
-  const program = setupProgram(vertexShader, fragmentShader);
+  const shaderProgram = initShaderProgram(vertexShader, fragmentShader);
 
-  // vertices of the triangle
-  const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(
+        shaderProgram,
+        "uProjectionMatrix"
+      ),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    },
+  };
 
-  // what is positionBuffer
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-  // what does this do
-  const positionAttributeLocation = gl.getAttribLocation(program, "position");
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
-  // clear to solid color :)
   clearCanvas();
 
-  // what is this magic
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  // Here's where we call the routine that builds all the
+  // objects we'll be drawing.
+  const buffers = initBuffers(gl);
+
+  // Draw the scene
+  drawScene(gl, programInfo, buffers);
 }
 
 const clearCanvas = () => {
